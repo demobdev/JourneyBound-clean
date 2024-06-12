219c83723eb51350bb8d18bcfdd1edaad403d1cb
Demo <167224721+demobdev@users.noreply.github.com>
2024-06-11 22:54:31 -0400

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Figure, Image } from "react-bootstrap";
+import { Container, Row, Col, Figure, Image } from "react-bootstrap";
 import BannerVideo from "../assets/videos/EyeofJourneyBound.mp4";
 import JBWhiteImage from "../assets/images/JB_white.png";
 import BaseLogo from "../assets/images/base-logo.png";
@@ -19,27 +19,25 @@ function Banner() {
         <source src={BannerVideo} type="video/mp4" />
       </video>
       <Container fluid="lg" className="d-flex flex-column align-items-center justify-content-center text-center">
-        <div className="banner-content">
-          <Figure className="hero-logo">
-            <Image src={JBWhiteImage} className="image jb-logo" />
-          </Figure>
-          <div className="hero-text-wrapper">
-            <h1 className="title font-family-AG">
-              a web3 psychedelic experience
-            </h1>
-            <div className="brand-logo">
-              <div className="brand-logo-item">
-                <h4 className="shortTitle">on</h4>
-                <Figure className="mb-0">
-                  <Image src={BaseLogo} className="image1" />
-                </Figure>
-              </div>
-              <div className="brand-logo-item">
-                <h4 className="shortTitle">by the</h4>
-                <Figure className="mb-0">
-                  <Image src={AtriumLogo} className="image2" />
-                </Figure>
-              </div>
+        <Figure className="mb-5 hero-logo">
+          <Image src={JBWhiteImage} className="image jb-logo" />
+        </Figure>
+        <div className="hero-text-wrapper">
+          <h1 className="title font-family-AG">
+            a web3 psychedelic experience
+          </h1>
+          <div className="brand-logo">
+            <div className="text-center">
+              <h4 className="shortTitle">on</h4>
+              <Figure className="mb-0">
+                <Image src={BaseLogo} className="image1" />
+              </Figure>
+            </div>
+            <div className="text-center">
+              <h4 className="shortTitle">by the</h4>
+              <Figure className="mb-0">
+                <Image src={AtriumLogo} className="image2" />
+              </Figure>
             </div>
           </div>
         </div>
