c61fef9b6e0a206fcf12cdb210ebf50629b9b4c0
Demo Bailey <demo@blockbusters.tech>
2024-06-19 04:21:10 -0400

    Fixed video behavior on mobile and updated Banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,38 +6,45 @@ import BaseLogo from "../assets/images/base-logo.png";
 import AtriumLogo from "../assets/images/Atrium-logo.png";
 
 function Banner() {
-return (
-<section className="banner-main position-relative overflow-hidden">
-<video autoPlay muted loop className="fullscreen-bg__video">
-<source src={BannerVideo} type="video/mp4" />
-</video>
-<Container
-     fluid="lg"
-     className="d-flex flex-column align-items-center justify-content-center text-center"
-   >
-<Figure className="mb-5 jb-logo">
-<Image src={JBWhiteImage} className="image jb-logo" />
-</Figure>
-<h1 className="title font-family-AG">
-a web3 psychedelic experience
-</h1>
-<Row className="brand-logo align-items-center justify-content-between mt-4 w-100">
-<Col className="text-left">
-<h4 className="shortTitle">on</h4>
-<Figure className="mb-0">
-<Image src={BaseLogo} className="image1" />
-</Figure>
-</Col>
-<Col className="text-right">
-<h4 className="shortTitle">by the</h4>
-<Figure className="mb-0">
-<Image src={AtriumLogo} className="image2" />
-</Figure>
-</Col>
-</Row>
-</Container>
-</section>
-);
+  return (
+    <section className="banner-main position-relative overflow-hidden">
+      <video
+        autoPlay
+        muted
+        loop
+        controls={false}
+        className="fullscreen-bg__video"
+        style={{ pointerEvents: "none" }}
+      >
+        <source src={BannerVideo} type="video/mp4" />
+      </video>
+      <Container
+        fluid="lg"
+        className="d-flex flex-column align-items-center justify-content-center text-center"
+      >
+        <Figure className="mb-5 jb-logo">
+          <Image src={JBWhiteImage} className="image jb-logo" />
+        </Figure>
+        <h1 className="title font-family-AG">
+          a web3 psychedelic experience
+        </h1>
+        <Row className="brand-logo align-items-center justify-content-between mt-4 w-100">
+          <Col className="text-left">
+            <h4 className="shortTitle">on</h4>
+            <Figure className="mb-0">
+              <Image src={BaseLogo} className="image1" />
+            </Figure>
+          </Col>
+          <Col className="text-right">
+            <h4 className="shortTitle">by the</h4>
+            <Figure className="mb-0">
+              <Image src={AtriumLogo} className="image2" />
+            </Figure>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
 
 export default Banner;
